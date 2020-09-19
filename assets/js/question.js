443dc9f012bf1d43e8bdd23f7ff2a52a8807b381
Ivailo <paradoxa624@gmail.com>
2020-09-19 14:33:50 +0300

    added answer shuffling, result update and tranfering to result.html

--- assets/js/question.js
+++ assets/js/question.js
@@ -6,8 +6,11 @@ const q_count = document.querySelector('.count');
 const q = document.querySelector('.question');
 const next_btn = document.querySelector('.next');
 
+let answers_arr = new Array;
 const answers = document.querySelector('.answers');
+
 let result = 0;
+let q_index = 0;
 
 let api_req = "https://opentdb.com/api.php" + queryString;
 fetch(api_req)
@@ -20,58 +23,107 @@ fetch(api_req)
         useAPIdata(questions);
     })
 
-function getAnswer(answer_choice) {
-
-    let answer = document.createElement('li');
-    answer.classList.add('answer');
-    let answer_text = document.createElement('p');
-    answer_text.classList.add('answer_text');
-    answer_text.innerHTML = answer_choice;
-    answer_text.value = answer_choice;
-
-    let answer_btn = document.createElement('input');
-    answer_btn.classList.add('marker');
-    answer_btn.type = 'radio';
-    answer_btn.name = 'q_marker'
-    answer.appendChild(answer_btn);
-    answer.appendChild(answer_text);
-    answers.appendChild(answer);
-
-}
-    
-function getAnswers (incorrect_answers, correct_answer) {
-    
-    getAnswer(correct_answer);
-
-    for (let index = 0; index <  incorrect_answers.length; index++) {
-        getAnswer(incorrect_answers[index]);
+function shuffleAnswers() {
+    for (let i = answers_arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [answers_arr[i], answers_arr[j]] = [answers_arr[j], answers_arr[i]];
     }
 }
 
-let q_index = 0;
+function getAnswers(incorrect_answers, correct_answer) {
+    answers_arr = incorrect_answers;
+    answers_arr.push(correct_answer);
+    shuffleAnswers();
+}
 
-function useAPIdata(questions) {
-    
+function appendAnswers(answer_choice) {
+
+    for (let index = 0; index < answers_arr.length; index++) {
+
+        let answer = document.createElement('li');
+        answer.classList.add('answer');
+        let answer_text = document.createElement('p');
+
+        answer_text.classList.add('answer_text');
+        answer_text.id = 'ans' + index;
+        answer_text.innerHTML = answers_arr[index];
+        answer_text.value = answers_arr[index];
+
+        let answer_btn = document.createElement('input');
+        answer_btn.classList.add('marker');
+        answer_btn.type = 'radio';
+        answer_btn.name = 'q_marker'
+        answer_btn.id = 'radio_a' + index;
+        answer.appendChild(answer_btn);
+        answer.appendChild(answer_text);
+        answers.appendChild(answer);
+    }
+}
+
+function addingEventListenerToAnswers() {
+    document.querySelectorAll('.answer').forEach(item => {
+        item.addEventListener('click', () => {
+          item.firstChild.checked = true;
+          next_btn.disabled = false;
+        })
+    })
+}
+
+function startQuiz(questions) {
     q_type.innerHTML = questions[q_index].category + ", " + questions[q_index].difficulty;
     q_count.innerHTML = (q_index + 1) + "/" + questions.length;
     q.innerHTML = questions[q_index].question; 
+}
+
+function initAnswers(questions) {
+
+    while(answers.firstChild){
+        answers.removeChild(answers.firstChild);
+    }
+
+    q_count.innerHTML = (q_index + 1) + "/" + questions.length;
+    q.innerHTML = questions[q_index].question;
     getAnswers(questions[q_index].incorrect_answers,questions[q_index].correct_answer);
+    appendAnswers();
+    addingEventListenerToAnswers();
+    next_btn.disabled = true;
+    
+}
+
+function checkAnswer(question) {
+    let answered_index = 0;
+    for (let index = 0; index < answers_arr.length; index++) {
+        if(document.getElementById('radio_a' + index).checked) {
+            answered_index = index;
+        }
+    }
+    
+    console.log(question.correct_answer);
+
+    if(document.getElementById('ans' + answered_index).innerHTML == question.correct_answer){
+        result++;
+    }
     q_index++;
+}
+
+
+function useAPIdata(questions) {
+    
+    startQuiz(questions);
+    initAnswers(questions);
+    checkAnswer(questions[q_index]);
 
     next_btn.addEventListener('click', () => {
-
+        
         if(q_index == questions.length) {
-            window.location = "./result.html";
+            console.log(result);
+            window.location = "./result.html?amount=" + 'result=' + result + '&total_questions=' + questions.length;
+            return;
         }
-        while(answers.firstChild){
-            answers.removeChild(answers.firstChild);
-        }
-
-        q_count.innerHTML = (q_index + 1) + "/" + questions.length;
-        q.innerHTML = questions[q_index].question;
-        getAnswers(questions[q_index].incorrect_answers,questions[q_index].correct_answer);
-
-        q_index++;
+        
+        initAnswers(questions);
+        checkAnswer(questions[q_index]);
+        
     })
 }
 
