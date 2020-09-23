eac907e7066d6d85afe50f6d24dc65026d750ccd
Nakahotty <naskuuu@abv.bg>
2020-09-23 13:17:25 +0300

    Implemented random difficulty

--- assets/js/quiz_form.js
+++ assets/js/quiz_form.js
@@ -53,6 +53,7 @@ let index = 0;
 let randomID = 0;
 let numOfQuestions = 0;
 
+
 fetch('https://opentdb.com/api_category.php')
         .then(res => {
             return res.json();
@@ -63,13 +64,22 @@ fetch('https://opentdb.com/api_category.php')
             randomID = categories[index].id;
             numOfQuestions = Math.floor(Math.random() * 9) + 1;
 
+            let randomDiff = '';
+            let randomDiffNumber = Math.floor(Math.random() * 3);
+                if (randomDiffNumber === 0) 
+                    randomDiff = 'easy';
+                else if (randomDiffNumber === 1)
+                    randomDiff = 'medium';
+                else 
+                    randomDiff = 'hard';
+
             const random_btn = document.querySelector('.random_btn');
             const random_nav_btn = document.querySelector('.random_nav_btn');
 
             random_btn.addEventListener('click', () => {
-                window.location = " ./question.html?amount=" + numOfQuestions + "&category=" + randomID + "&difficulty=easy";
+                window.location = " ./question.html?amount=" + numOfQuestions + "&category=" + randomID + "&difficulty=" + randomDiff;
             })
             random_nav_btn.addEventListener('click', () => {
-                window.location = " ./question.html?amount=" + numOfQuestions + "&category=" + randomID + "&difficulty=easy";
+                window.location = " ./question.html?amount=" + numOfQuestions + "&category=" + randomID + "&difficulty=" + randomDiff;
             })
         })
